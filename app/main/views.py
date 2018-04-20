bbd54e887c90daaf1052af0f4022d9986e81784d
muturi254 <petermuturi74@gmail.com>
2018-04-20 13:44:29 +0300

    updated view and navbar to alow change of class active

--- app/main/views.py
+++ app/main/views.py
@@ -4,4 +4,12 @@ from . import main
 
 @main.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', active="home")
+
+@main.route('/about')
+def about():
+    return render_template('about.html',  active="about")
+
+@main.route('/contact')
+def contact():
+    return render_template('contact.html', active="contact")
