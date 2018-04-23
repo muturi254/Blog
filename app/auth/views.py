edec4818a22e6c3b2729c90a4aa9c8e9b5c4836e
muturi254 <petermuturi74@gmail.com>
2018-04-23 06:27:59 +0300

    set up views for the authentication blueprint: login and signup

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,6 +1,15 @@
-from . import main
+from flask import render_template
+from . import auth
+from .forms import LoginForm, RegisterForm
 
-@main.route('/')
-def index():
-    return '<h1> Hello World </h1>'
 
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    login = LoginForm()
+    return render_template('auth/login.html', login = login, active = 'login')
+
+
+@auth.route('/signup', methods= ['GET', 'POST'])
+def signup():
+    signup = RegisterForm()
+    return render_template('auth/signup.html', signup= signup, active = 'signup')
