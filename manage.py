714d74e45859fb6a12beb1cd5a59441d51838b68
muturi254 <petermuturi74@gmail.com>
2018-04-23 16:49:20 +0300

    added migrate capabilities

--- manage.py
+++ manage.py
@@ -1,6 +1,7 @@
 from flask_script import Manager,Server
 from app import create_app,db
 from app.models import User
+from flask_migrate import Migrate, MigrateCommand
 
 app = create_app('default')
 
@@ -8,6 +9,8 @@ manager = Manager(app)
 
 manager.add_command('server', Server)
 
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
 
 @manager.shell
 def make_shell_context():
