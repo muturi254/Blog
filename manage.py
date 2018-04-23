a7f6160debe101453ee3ede4bae1e27013b44074
muturi254 <petermuturi74@gmail.com>
2018-04-23 11:28:58 +0300

    included shell context for cli capabilities

--- manage.py
+++ manage.py
@@ -1,11 +1,17 @@
 from flask_script import Manager,Server
 from app import create_app,db
+from app.models import User
 
 app = create_app('default')
 
 manager = Manager(app)
 
 manager.add_command('server', Server)
+
+
+@manager.shell
+def make_shell_context():
+    return dict(app=app, db=db, User=User)
 
 if __name__ == '__main__':
     manager.run()
