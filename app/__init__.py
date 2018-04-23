968746ec0d652f2fb41a4846c8180092f44d6032
muturi254 <petermuturi74@gmail.com>
2018-04-23 06:33:37 +0300

    set up authentication and blog on register app

--- app/__init__.py
+++ app/__init__.py
@@ -16,7 +16,15 @@ def create_app(config_state):
     bootstrap.init_app(app)
     db.init_app(app)
 
+    # register blueprints
+        # main blue print
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+        # auth blueprint
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
+        # blog blue print
+    from . Blog import blog as blog_blueprint
+    app.register_blueprint(blog_blueprint)
 
     return app
