21a6a3f79e2a611f073ec3cff3539dea731e7e7d
muturi254 <petermuturi74@gmail.com>
2018-04-23 06:36:31 +0300

    added both login ang register forms

---
+++ app/auth/forms.py
@@ -0,0 +1,21 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, SubmitField, PasswordField
+from wtforms.validators import Required, Length, Email, EqualTo
+
+class LoginForm(FlaskForm):
+    username = StringField('username', validators=[Required()])
+    pasword = PasswordField('Password', validators=[Required()])
+    submit = SubmitField('Login')
+
+
+
+class RegisterForm(FlaskForm):
+    firstname = StringField('Firstname', validators=[Required()])
+    lastname = StringField('Lastname', validators=[Required()])
+    username = StringField('Username', validators=[Required()])
+    email = StringField('Emil', validators=[Required(), Email()])
+    password = PasswordField('Password', validators=[Required(),
+                                                      EqualTo('password_confirm', message='Passwords must match')])
+    password_confirm = PasswordField(
+        'Confirm Passwords', validators=[Required()])
+    submit = SubmitField('SignUp')
