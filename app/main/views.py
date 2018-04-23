90d4c48e45af38e8265a70481368de045e851c30
muturi254 <petermuturi74@gmail.com>
2018-04-23 06:31:34 +0300

    added view page for index, about and contact

--- app/main/views.py
+++ app/main/views.py
@@ -1,15 +1,16 @@
-from flask import render_template
 from . import main
-
+from flask import render_template
+from .forms import ContactForm
 
 @main.route('/')
 def index():
-    return render_template('index.html', active="home")
-
-@main.route('/about')
-def about():
-    return render_template('about.html',  active="about")
+    return render_template('index.html', active = 'home')
 
 @main.route('/contact')
 def contact():
-    return render_template('contact.html', active="contact")
+    form = ContactForm()
+    return render_template('contact.html', form=form, active =  'contact')
+
+@main.route('/about')
+def about():
+    return render_template('about.html')
